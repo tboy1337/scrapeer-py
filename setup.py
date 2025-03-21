28b73ab1c209df015b93bf23b44005d0e518e963
tboy1337 <30571311+tboy1337@users.noreply.github.com>
2025-03-21 17:27:48 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="scrapeer",
+    version="0.1.0",
+    description="Essential Python library that scrapes HTTP(S) and UDP trackers for torrent information.",
+    author="Python Port of TorrentPier's Scrapeer",
+    author_email="",
+    url="https://github.com/tboy1337/scrapeer-py",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    keywords=[
+        "torrent",
+        "torrents",
+        "scraper",
+        "torrent-scraper",
+        "torrent-scrap",
+        "torrent-scraping"
+    ],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+)
